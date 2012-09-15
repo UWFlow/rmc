c38eb970d9ac6d6372345f7a741b04e638b15e8e
Jiayi Sandy Wu <jiayisandywu@gmail.com>
2012-09-15 04:21:35 -0400

    Eng. course critique data fix.
      * Fixed typo that ended up not storing some extra data
      * Added comments about the magic/messy DOM crawling
      * Newly crawled results not stored to keep repo small. Though shouldn't take
        longer than 15-30 minutes to fetch from the web

--- data/evals/scraper.py
+++ data/evals/scraper.py
@@ -19,7 +19,7 @@ output_dir = "output/"
 output_file_name = output_dir + "results_" + cur_time + ".txt"
 error_file_name = output_dir + "error_" + cur_time + ".txt"
 
-# Add on password stuff
+# Use credentials
 password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
 top_level_url = "https://www.eng.uwaterloo.ca/critiques/index.php"
 password_mgr.add_password(None, top_level_url, username, password)
@@ -28,7 +28,7 @@ opener = urllib2.build_opener(handler)
 opener.open(top_level_url)
 urllib2.install_opener(opener)
 
-# Grab local file for request data and list of all possible courses
+# Grab local file for post headers and list of all possible courses
 url = "https://www.eng.uwaterloo.ca/critiques/index.php"
 file = open("post_headers.txt","r")
 post_headers = pickle.load(file)
@@ -47,6 +47,7 @@ for course in courses:
 	course_code = course[:last_space]
 	course_num = course[last_space + 1:]
 
+# Fetch the results page
 	data = urllib.urlencode(post_headers)
 	req = urllib2.Request(url, data)
 	response = urllib2.urlopen(req)
@@ -54,6 +55,15 @@ for course in courses:
 
 	doc = the_page
 	soup = BeautifulSoup(doc)
+
+# We're assuming the following format:
+# #primarycontent containts all results.
+# For each result we have the following:
+#	<table> with 1 <tr> containing term, year, instructor, course code
+#	<table> with 21 <tr>'s containing main critique content
+#	<table> with 1 <tr> containing total replies and class size
+
+
 	litmus = soup.findAll(attrs={"id":"primarycontent"})
 
 	if not litmus[0].contents[1].contents[0] == "No results were found.":
@@ -65,7 +75,7 @@ for course in courses:
 		}
 
 		critique = {}
-		data = {}
+		scores = {}
 
 		result_length = len(result);
 		if result_length >= 230:
@@ -74,6 +84,7 @@ for course in courses:
 			err_file.write(course + " got more than 10 results (" + str(result_length) + ")\n")
 
 		for i in range(0, result_length):
+# The <tr> in the first <table>
 			if (i % 23) == 0:
 				times = result[i].contents[1].contents[0].lstrip("Term: ").rstrip().split();
 				prof = result[i].contents[3].contents[0].lstrip("Instructor: ").rstrip();
@@ -82,25 +93,27 @@ for course in courses:
 					"term": times[0],
 					"year": times[1]
 				}
-				data = {}
+				scores = {}
 			elif (i % 23) == 22:
-				critique["class_size"] = int(result[22].contents[5].contents[0].lstrip("Class Size: "))
-				critique["total_replies"] = int(result[22].contents[3].contents[0].lstrip("Total replies: "))
-				critique["data"] = data
+# The <tr> in the last <table>
+				critique["class_size"] = int(result[i].contents[5].contents[0].lstrip("Class Size: "))
+				critique["total_replies"] = int(result[i].contents[3].contents[0].lstrip("Total replies: "))
+				critique["scores"] = scores
 
-				course_dict["critiques"].append(data)
+				course_dict["critiques"].append(critique)
 			elif len(result[i]) == 20:
+# The interesting rows of result[i] just happen to have 20 elements in .contents
 				q = int(result[i].contents[0].contents[0].contents[0])
-				data[q] = {}
-				data[q]["replies"] = int(result[i].contents[2].contents[0])
+				scores[q] = {}
+				scores[q]["num_replies"] = int(result[i].contents[2].contents[0])
 
-				data[q]["A"] = int(result[i].contents[6].contents[0])
-				data[q]["B"] = int(result[i].contents[8].contents[0])
-				data[q]["C"] = int(result[i].contents[10].contents[0])
-				data[q]["D"] = int(result[i].contents[12].contents[0])
-				data[q]["E"] = int(result[i].contents[14].contents[0])
+				scores[q]["A"] = int(result[i].contents[6].contents[0])
+				scores[q]["B"] = int(result[i].contents[8].contents[0])
+				scores[q]["C"] = int(result[i].contents[10].contents[0])
+				scores[q]["D"] = int(result[i].contents[12].contents[0])
+				scores[q]["E"] = int(result[i].contents[14].contents[0])
 
-				data[q]["average"] = int(result[i].contents[18].contents[0])
+				scores[q]["average"] = int(result[i].contents[18].contents[0])
 
 		file.write(str(course_dict)+"\n")
 		print "Data fetched and saved!"
