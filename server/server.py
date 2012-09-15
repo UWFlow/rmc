23c82085cb7a1291dfc3e1ea2f121446a23c09d5
mduan <duan.mack@gmail.com>
2012-09-15 18:27:26 -0400

    create course search api for searching and sorting courses

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 import flask
 import pymongo
+import re
 
 import rmc.shared.constants as c
 
@@ -19,6 +20,9 @@ def profile(fbid):
         # TODO(mack): handle viewing another user's profile
         pass
 
+@app.route('/courses')
+def courses():
+    return flask.render_template('course_page.html')
 
 # TODO(mack): move API's to separate file
 # TODO(mack): add security measures (e.g. require auth, xsrf cookie)
@@ -38,26 +42,73 @@ def get_courses(course_names):
       {'_id': 0}
     ))
 
-    def clean_courses(course):
-        return {
-            'id': course['name'],
-            'name': course['title'],
-            'rating': round(course['ratings']['aggregate']['average']*10)/10,
-            'numRatings': course['ratings']['aggregate']['count'],
-            'description': course['description'],
-            'availFall': bool(int(course['availFall'])),
-            'availSpring': bool(int(course['availSpring'])),
-            'availWinter': bool(int(course['availWinter'])),
-            # TODO(mack): get actual number for this
-            'numFriendsTook': random.randrange(0, 20),
-        }
     # TODO(mack): do this more cleanly
-    courses_list = map(clean_courses, courses_list)
+    courses_list = map(clean_course, courses_list)
     courses = {}
     for course in courses_list:
         courses[course['id']] = course
 
     return flask.jsonify(courses=courses)
+
+COURSES_SORT_MODES = {
+    # TODO(mack): 'num_friends'
+    'num_ratings': {'dir': pymongo.DESCENDING, 'field': 'ratings.count'},
+    'overall': {'dir': pymongo.DESCENDING, 'field': 'ratings.aggregate.average'},
+    'interest': {'dir': pymongo.DESCENDING, 'field': 'ratings.interest.average'},
+    'easiness': {'dir': pymongo.DESCENDING, 'field': 'ratings.easy.average'},
+}
+
+@app.route('/api/course-search', methods=['GET'])
+# TODO(mack): find a better name for function
+def search_courses():
+    # TODO(mack): create enum of sort options
+    # num_friends, num_ratings, overall, interest, easiness
+
+    request = flask.request
+    search = request.values.get('search')
+    sort_by = request.values.get('sort_by', 'num_ratings')
+    count = request.values.get('count', 10)
+    offset = request.values.get('offset', 0)
+
+    filters = []
+    if search:
+        search_re = re.compile(search, re.IGNORECASE)
+        search_filters = []
+        for field in ['name', 'title']:
+            search_filters.append({field: {'$regex': search_re}})
+        filters.append({'$or': search_filters})
+    if len(filters) > 0:
+        unsorted_courses = db.courses.find({'$and': filters})
+    else:
+        unsorted_courses = db.courses.find()
+
+    sort_options = COURSES_SORT_MODES[sort_by]
+    sorted_courses = unsorted_courses.sort(
+        sort_options['field'],
+        # TODO(mack): add sort direction
+        direction=sort_options['dir']
+    )
+    limited_courses = sorted_courses.skip(offset).limit(count)
+    courses = map(clean_course, limited_courses)
+    return flask.jsonify(courses=courses)
+
+
+# Helper functions
+
+def clean_course(course):
+    return {
+        'id': course['name'],
+        'name': course['title'],
+        'rating': round(course['ratings']['aggregate']['average']*10)/10,
+        'numRatings': course['ratings']['aggregate']['count'],
+        'description': course['description'],
+        'availFall': bool(int(course['availFall'])),
+        'availSpring': bool(int(course['availSpring'])),
+        'availWinter': bool(int(course['availWinter'])),
+        # TODO(mack): get actual number for this
+        'numFriendsTook': random.randrange(0, 20),
+    }
+
 
 if __name__ == '__main__':
   app.debug = True
