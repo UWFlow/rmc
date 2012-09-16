db6639cc343b910c9dbb704447d28d9e2f85df3b
David Hu <davidhu91@gmail.com>
2012-09-15 22:48:19 -0400

    add expanded review

--- server/static/js/review.js
+++ server/static/js/review.js
@@ -1,6 +1,6 @@
 define(
 ['ext/backbone', 'ext/jquery', 'ext/underscore', 'ext/underscore.string',
-'ratings', 'ext/select2'],
+'ratings', 'ext/select2', 'ext/autosize'],
 function(Backbone, $, _, _s, ratings, select2) {
 
   // TODO(david): May want to refactor to just a UserCourse model
@@ -24,6 +24,10 @@ function(Backbone, $, _, _s, ratings, select2) {
   });
 
   var UserReviewView = Backbone.View.extend({
+    events: {
+      'change .prof-select': 'showReview'
+    },
+
     initialize: function(options) {
     },
 
@@ -33,10 +37,18 @@ function(Backbone, $, _, _s, ratings, select2) {
 
       // TODO(david): Make this prettier and conform to our styles
       // TODO(david): Allow adding a prof
-      this.$('.professor-select').select2({
+      this.$('.prof-select').select2({
       });
 
+      this.$('.comments')
+        .autosize()
+        .css('resize', 'none');
+
       return this;
+    },
+
+    showReview: function() {
+      this.$('.review-details').slideDown();
     }
   });
 
