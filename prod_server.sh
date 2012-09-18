d664a128135f31358904a47777a527a41c88ef9e
David Hu <davidhu91@gmail.com>
2012-09-18 04:59:40 -0400

    temporary prod_server daemonize

--- prod_server.sh
+++ prod_server.sh
@@ -11,5 +11,7 @@ cd /home/rmc
 compass compile rmc/server/static/
 
 # Start the uwsgi server
+# TODO(david): Benchmark with ab and use worker processes if necessary
+# TODO(david): Run as a daemon so will start on boot and can easily restart
 uwsgi -s /tmp/uwsgi.sock -w rmc.server.server:app --chmod-socket 666 \
-  --logto /home/rmc/logs/uwsgi
+  --logto /home/rmc/logs/uwsgi --daemonize
