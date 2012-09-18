8ac0e259e44aadaa64b83999f47ffd3c568ec47e
Jiayi Sandy Wu <jiayisandywu@gmail.com>
2012-09-18 12:44:05 -0400

    Moved landing page js to it's own file

---
+++ server/static/js/index_page.js
@@ -0,0 +1,23 @@
+require(
+['ext/jquery'],
+function($) {
+  $(function() {
+    window.fbAsyncInit = function() {
+      FB.init({appId: '219309734863464', status: true, cookie: true, xfbml: true});
+
+      FB.Event.subscribe('auth.login', function(response) {
+        if (response.status === 'connected') {
+          window.location.href = '/profile'
+        }
+      });
+    };
+    (function() {
+      var e = document.createElement('script');
+      e.type = 'text/javascript';
+      e.src = document.location.protocol +
+          '//connect.facebook.net/en_US/all.js';
+      e.async = true;
+      document.getElementById('fb-root').appendChild(e);
+    })();
+  });
+});
