a9ff5d878283a4947eea1c91ec1a62adc4b3d7bd
David Hu <davidhu91@gmail.com>
2012-09-25 19:01:54 -0400

    screw it, just run uwsgi as root

--- prod_server.sh
+++ prod_server.sh
@@ -13,9 +13,9 @@ cd /home/rmc
 # TODO(david): Benchmark with ab and use worker processes if necessary
 uwsgi \
   --socket /tmp/uwsgi.sock \
+  --chmod-socket 666 \
   --env FLASK_CONFIG=/home/rmc/rmc/config/flask_prod.py \
   --wsgi rmc.server.server:app \
   --master \
   --daemonize /home/rmc/logs/uwsgi.log \
-  --uid rmc \
   --pidfile $1
