acdc2689fed7b07c7634c6fa7c3483a2fdfc0577
David Hu <davidhu91@gmail.com>
2012-09-25 01:27:35 -0400

    style basic review text and pic (speech bubble and doggy display pic)

--- server/static/js/comment.js
+++ server/static/js/comment.js
@@ -8,18 +8,30 @@ function(Backbone, $, _, util) {
       user_id: null,
       fbid: null,
       comment: '',
-      comment_date: new Date(0)
+      comment_date: new Date(0),
+      author_pic_url: ''
     },
 
     initialize: function(attributes) {
       if (attributes) {
         this.set('comment_date', util.toDate(attributes.comment_date));
       }
+
+      if (!attributes || !attributes.author_pic_url) {
+        var attrs = attributes ? attributes : this.defaults;
+        var size = [
+            util.getHashCode('' + this.get('comment_date')) % 20 + 80,
+            util.getHashCode(attrs.comment) % 20 + 80
+        ];
+        this.set('author_pic_url',
+            'http://placedog.com/' + size[0] + '/' + size[1]);
+      }
     }
   });
 
   var CommentView = Backbone.View.extend({
     template: _.template($('#comment-tpl').html()),
+    className: 'comment',
 
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
