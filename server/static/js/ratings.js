6c95bf1b7dfb222cced67588873b56a6ee81647f
David Hu <davidhu91@gmail.com>
2012-10-04 01:50:33 -0400

    remove old 5-bars (stars) input ratings code

--- server/static/js/ratings.js
+++ server/static/js/ratings.js
@@ -64,42 +64,21 @@ function(RmcBackbone, $, _, _s, util, _bootstrap) {
   var RatingCollection = RmcBackbone.Collection.extend({
     model: RatingModel,
 
-    getNameAt: function(index) {
-      return this.at(index).get('name');
-    },
-
     getRating: function(name) {
       return this.where({ name: name })[0];
     }
-
   });
 
   var RatingsView = RmcBackbone.View.extend({
 
-    events: {
-      //'mouseenter .rating-progress': 'onRatingHover',
-      //'mouseleave .input-rating': 'setUserRatings',
-      //'click .rating-progress': 'onRatingClick'
-    },
-
     initialize: function(options) {
       this.ratings = options.ratings;
       this.userCourse = options.userCourse;
-      this.editOnly = options.editOnly;
-      this.readOnly = options.readOnly;
-
-      if (!this.readOnly) {
-        this.userCourse.on('change', this.setUserRatings, this);
-      }
     },
 
     render: function() {
       var ratings = this.ratings;
-      this.$el.html(_.template($('#ratings-tpl').html(), {
-        ratings: ratings,
-        editOnly: this.editOnly,
-        readOnly: this.readOnly
-      }));
+      this.$el.html(_.template($('#ratings-tpl').html(), { ratings: ratings }));
 
       this.$('[title]').tooltip();
 
@@ -111,57 +90,8 @@ function(RmcBackbone, $, _, _s, util, _bootstrap) {
       return this;
     },
 
-    setUserRatings: function() {
-      if (this.readOnly) {
-        return;
-      }
-
-      var self = this;
-      this.$('.input-rating').each(function(i, inputRating) {
-        var name = self.ratings.getNameAt(i);
-        var userRating = self.userCourse.getRating(name);
-        var value = userRating ? userRating * NUM_SEGMENTS : 0;
-        self.selectRating(inputRating, value);
-      });
-    },
-
     removeBars: function() {
       this.$('.bar').remove();
-    },
-
-    // TODO(david): Refactor to use single model+view for each rating
-    onRatingHover: function(evt) {
-      if (this.readOnly) {
-        return;
-      }
-
-      this.setUserRatings();
-
-      var $target = $(evt.currentTarget);
-      var $rowElem = $target.closest('.row-fluid');
-      var value = $target.index() + 1;
-      this.selectRating($rowElem.find('.input-rating'), value);
-    },
-
-    selectRating: function(inputRatingElem, value) {
-      var numberValue = value === 0 ? '' : value;
-      $(inputRatingElem)
-        .find('.rating-bar').each(function(i, elem) {
-          $(elem).toggleClass('bar', i < value).css('opacity', '');
-        })
-        .closest('.row-fluid').find('.input-rating-num').text(numberValue);
-    },
-
-    onRatingClick: function(evt) {
-      if (this.readOnly) {
-        return;
-      }
-      var $target = $(evt.currentTarget);
-      var index = $target.closest('.row-fluid').index();
-      var name = this.ratings.getNameAt(index);
-      var value = ($target.index() + 1) / NUM_SEGMENTS;
-      this.userCourse.setRating(name, value);
-      $target.parent().find('.bar').css('opacity', 1.0);
     }
 
   });
