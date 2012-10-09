8c9b30a601eba96ddc43f64f815d93624d368eff
David Hu <davidhu91@gmail.com>
2012-10-09 03:35:06 -0400

    redesign homepage: SLC header background image with simple 2-sentence text

--- server/static/js/index_page.js
+++ server/static/js/index_page.js
@@ -1,5 +1,7 @@
 require(
-['facebook'],
-function(facebook) {
-  facebook.loginIfPossible();
+['facebook', 'ext/jquery'],
+function(facebook, $) {
+  //facebook.loginIfPossible();
+  $('.header-bg').css('opacity', 1.0);
+  $('.sign-up-box').addClass('animated');
 });
