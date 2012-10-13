eefb2c48d6654d75889f1aec28df2fb644907ab5
David Hu <davidhu91@gmail.com>
2012-10-13 00:17:54 -0400

    facebook connect button "see hwat your friends are taking" A/B test

--- server/static/js/index_page.js
+++ server/static/js/index_page.js
@@ -7,6 +7,11 @@ function(facebook, $, util) {
     facebook.loginIfPossible();
   }
 
+  // Facebook connect button A/B test
+  if (util.getQueryParam('ft')) {
+    $('.fb-login-text').text('See what friends are taking');
+  }
+
   $('.header-bg').css('opacity', 1.0);
   $('.sign-up-box').addClass('animated');
 });
