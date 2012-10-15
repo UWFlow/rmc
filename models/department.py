73201ae6e926551d5a642f367135c54f2a2c0a88
mduan <duan.mack@gmail.com>
2012-10-14 21:38:53 -0400

    add TODO to Department model

--- models/department.py
+++ models/department.py
@@ -11,5 +11,7 @@ class Department(me.Document):
     # eg. sci
     faculty_id = me.StringField(required=True)
 
+    # TODO(mack): url is broken, a lot of urls are for the grad rather
+    # than undergrad calendar
     # eg. http://ugradcalendar.uwaterloo.ca/courses/EARTH
     url = me.URLField(required=True)
