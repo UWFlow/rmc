b7c0754d23aceb1300a661cfa2e9abb6cd4762bc
David Hu <davidhu91@gmail.com>
2012-10-18 02:54:51 -0400

    kill "See what friend are taking" FB button A/B test... does not seem to be winning out

--- server/static/js/index_page.js
+++ server/static/js/index_page.js
@@ -7,11 +7,6 @@ function(_facebook, $, util) {
     _facebook.loginIfPossible('HOME_AUTO');
   }
 
-  // Facebook connect button A/B test
-  if (util.getQueryParam('ft')) {
-    $('.fb-login-text').text('See what friends are taking');
-  }
-
   $('.header-bg').css('opacity', 1.0);
   $('.sign-up-box').addClass('animated');
 
