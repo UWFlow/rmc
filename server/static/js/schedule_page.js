f7927149b06d92e7b72e6b71047256c11e71ad3c
mduan <duan.mack@gmail.com>
2013-01-02 23:06:54 -0500

    make view profile & connect to view profile buttons work on schedule page

--- server/static/js/schedule_page.js
+++ server/static/js/schedule_page.js
@@ -3,8 +3,8 @@
 // require('user_course') in the 'course' module); need to investigate further
 require(
 ['ext/jquery', 'ext/underscore', 'ext/underscore.string', 'user', 'course',
-'user_course', 'schedule'],
-function($, _, _s, _user, _course, _user_course, _schedule) {
+'user_course', 'schedule', 'facebook'],
+function($, _, _s, _user, _course, _user_course, _schedule, _facebook) {
 
   _user.UserCollection.addToCache(pageData.userObjs);
   _course.CourseCollection.addToCache(pageData.courseObjs);
@@ -19,5 +19,11 @@ function($, _, _s, _user, _course, _user_course, _schedule) {
   });
   $schedulePlaceholder.replaceWith(scheduleView.el);
 
-  mixpanel.track('Impression: Profile page');
+  _facebook.initConnectButton({
+    $button: $('.fbconnect-btn'),
+    source: 'VIEW_FULL_PROFILE_SCHEDULE_PAGE',
+    nextUrl: '/profile/' + pageData.profileUserId.$oid
+  });
+
+  mixpanel.track('Impression: Schedule page');
 });
