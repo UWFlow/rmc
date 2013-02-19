7ac9dec4987921d1cc57aa372338e22c7dc42cf9
David Hu <davidhu91@gmail.com>
2013-02-18 23:26:49 -0800

    spawn 4 uWSGI worker processes so one long mongo query doesn't hang the entire site (eg. /dashboard)

--- prod_server.sh
+++ prod_server.sh
@@ -10,13 +10,13 @@ set -e
 cd /home/rmc
 
 # Start the uwsgi server
-# TODO(david): Benchmark with ab and use worker processes if necessary
 uwsgi \
   --socket /tmp/uwsgi.sock \
   --chmod-socket 666 \
   --env FLASK_CONFIG=/home/rmc/rmc/config/flask_prod.py \
   --wsgi rmc.server.server:app \
   --master \
+  --workers 4 \
   --daemonize /home/rmc/logs/uwsgi.log \
   --pidfile $1
 
