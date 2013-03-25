3c204698997fe3e6a22e7daa7bfe1da0dfe3e0a7
David Hu <davidhu91@gmail.com>
2013-03-25 03:19:32 -0700

    add an alert to the homepage indicating sign-up now can win prize

--- server/static/js/index_page.js
+++ server/static/js/index_page.js
@@ -1,6 +1,6 @@
 require(
-['facebook', 'ext/jquery', 'util', 'sign_in', 'ext/cookie'],
-function(_facebook, $, _util, _sign_in, __) {
+['facebook', 'ext/jquery', 'util', 'sign_in', 'ext/cookie', 'ext/moment'],
+function(_facebook, $, _util, _sign_in, _cookie, _moment) {
   $('.header-bg').css('opacity', 1.0);
   $('.sign-up-box').addClass('animated');
 
@@ -31,4 +31,16 @@ function(_facebook, $, _util, _sign_in, __) {
   mixpanel.track('Impression: Landing page');
 
   $(document.body).trigger('pageScriptComplete');
+
+  window.setTimeout(function() {
+    $('#referral-contest').fadeIn(1000);
+  }, 500);
+
+  window.setInterval(function() {
+    var duration = moment.duration(moment('2013-03-25 23:59:59 -0400').diff());
+    $('#referral-contest')
+      .find('.contest-hours').text(duration.hours()).end()
+      .find('.contest-mins').text(duration.minutes()).end()
+      .find('.contest-secs').text(duration.seconds()).end();
+  }, 1000);
 });
