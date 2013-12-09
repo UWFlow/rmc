b0c459a70483f3b83f2b4a646b84022d05d4332d
Jamie Wong <jamie.lf.wong@gmail.com>
2013-12-09 00:46:46 -0500

    Add in missing underscore dep to rmc_moment.js

--- server/static/js/rmc_moment.js
+++ server/static/js/rmc_moment.js
@@ -1,4 +1,4 @@
-define(["moment", "moment-timezone", "ext/moment-timezone-data"], function (_moment, __, __) {
+define(["ext/underscore", "moment", "moment-timezone", "ext/moment-timezone-data"], function (_, _moment, __, __) {
   // Always use America/Toronto as the timezone.
   function rmcMoment(a, b, c, d) {
     return _moment(a, b, c, d)
