bac61fea003df0633af0a28ec6678a4a70de821b
Jamie Wong <jamie.lf.wong@gmail.com>
2014-02-17 16:48:17 -0500

    Kitten scripts

---
+++ kittens/add_from_flickr.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+from StringIO import StringIO
+import os
+import re
+import sys
+
+import requests
+from PIL import Image
+
+import rmc.shared.secrets as s
+import rmc.shared.constants as c
+import rmc.kittens.data as kitten_data
+
+
+def get_photo_info_from_flickr(photo_id):
+    print >>sys.stderr, 'Getting information about photo id:', photo_id
+    url = ("http://api.flickr.com/services/rest/?method=flickr.photos.getInfo"
+           "&api_key=%s"
+           "&photo_id=%s"
+           "&format=json"
+           "&nojsoncallback=1") % (s.FLICKR_API_KEY, photo_id)
+
+    return requests.get(url).json["photo"]
+
+COLOR_WIDTH = 150
+COLOR_HEIGHT = 150
+GREY_WIDTH = 50
+GREY_HEIGHT = 50
+
+BASE_OUTDIR = os.path.join(c.RMC_ROOT, 'server', 'static', 'img', 'kittens')
+
+
+def download_photo(photo_info, index):
+    photo_url = ('http://farm%(farm_id)s.staticflickr.com/'
+                 '%(server_id)s/%(photo_id)s_%(secret_id)s.jpg') % {
+                    'farm_id': photo_info['farm'],
+                    'server_id': photo_info['server'],
+                    'photo_id': photo_info['id'],
+                    'secret_id': photo_info['secret'],
+                }
+
+    print >>sys.stderr, 'Downloading', photo_url, '...'
+    photo_content = requests.get(photo_url).content
+    color_img = Image.open(StringIO(photo_content))
+
+    width, height = color_img.size
+
+    min_dim = min(width, height)
+
+    # Crop to square
+    crop_box = (
+        (width - min_dim) / 2,
+        (height - min_dim) / 2,
+        width - (width - min_dim) / 2,
+        height - (height - min_dim) / 2
+    )
+    color_img = color_img.crop(crop_box)
+
+    try:
+        grey_img = color_img.copy().convert('L')
+    except IOError:
+        print >>sys.stderr
+        print >>sys.stderr, 'WARNING! You might be missing libjpeg.'
+        print >>sys.stderr, 'On OSX:     brew install libjpeg'
+        print >>sys.stderr, 'On Ubuntu:  sudo apt-get install libjpeg-dev'
+        print >>sys.stderr, 'On Fedora:  yum install libjpeg-devel'
+        print >>sys.stderr
+        print >>sys.stderr, 'After you have libjpeg installed:'
+        print >>sys.stderr, 'pip uninstall Pillow'
+        print >>sys.stderr, 'pip install -r requirements.txt'
+        print >>sys.stderr
+        raise
+
+    color_img.thumbnail((COLOR_WIDTH, COLOR_HEIGHT), Image.ANTIALIAS)
+    grey_img.thumbnail((GREY_WIDTH, GREY_HEIGHT), Image.ANTIALIAS)
+
+    color_img_path = os.path.join(BASE_OUTDIR, 'color', '%d.png' % index)
+    grey_img_path = os.path.join(BASE_OUTDIR, 'grey', '%d.png' % index)
+
+    color_img.save(color_img_path)
+    print >>sys.stderr, 'Saved', os.path.normpath(color_img_path)
+    grey_img.save(grey_img_path)
+    print >>sys.stderr, 'Saved', os.path.normpath(grey_img_path)
+
+
+new_flickr_url = sys.argv[1]
+new_photo_id = re.compile('\d+').findall(new_flickr_url)[0]
+new_photo_info = get_photo_info_from_flickr(new_photo_id)
+
+index = kitten_data.add_kitten_data(new_photo_info)
+download_photo(new_photo_info, index)
---
+++ kittens/data.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+data_path = os.path.join(os.path.dirname(__file__), 'data.json')
+
+
+def _get_kitten_data():
+    kitten_file = open(data_path, 'r')
+    kitten_data = json.load(kitten_file)
+    kitten_file.close()
+    return kitten_data
+
+
+def add_kitten_data(new_flickr_info):
+    """Add the provided flickr API response info to the data list.
+
+    If it's already in the data list, update it.
+
+    Return the index of the data in the list.
+    """
+    kitten_data = _get_kitten_data()
+
+    for (i, kitten_info) in enumerate(kitten_data):
+        if kitten_info['id'] == new_flickr_info['id']:
+            kitten_data[i] = new_flickr_info
+            index = i
+            break
+    else:
+        # Break didn't hit, the photo info is new
+        index = len(kitten_data)
+        kitten_data.append(new_flickr_info)
+
+    kitten_file = open(data_path, 'w')
+    json.dump(kitten_data, kitten_file, indent=2)
+    kitten_file.close()
+    return index
