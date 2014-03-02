090b36ae8bc49146afef49050c93edaa74dfab52
David Hu <davidhu91@gmail.com>
2014-03-02 17:19:30 -0500

    Notify the public room on deploy as well
    ... since we get GitHub notifications there, and it's useful to see deploy
    notifications interleaved with what's happening on GitHub.
    
    Resolves #79
    
    Test plan: Ran `PYTHONPATH=.. python notify_deploy.py 'david test'` with the
    room ID of a private test room for `HIPCHAT_PUBLIC_ROOM_ID`.

--- notify_deploy.py
+++ notify_deploy.py
@@ -14,22 +14,28 @@ from rmc.shared import secrets
 HIPCHAT_API_URL = 'https://api.hipchat.com/v1/rooms/message'
 
 
-if __name__ == '__main__':
-    deployer = sys.argv[1] if len(sys.argv) >= 2 else "rmc"
-    msg = "%s just deployed to uwflow.com" % deployer
+def notify_hipchat(deployer, room_id):
+    message = '%s just deployed to uwflow.com' % deployer
 
     payload = {
         'auth_token': secrets.HIPCHAT_TOKEN,
         'notify': 0,
         'color': 'purple',
         'from': 'Mr Monkey',
-        'room_id': secrets.HIPCHAT_HACK_ROOM_ID,
-        'message': msg,
+        'room_id': room_id,
+        'message': message,
         'message_format': 'text',
     }
 
     try:
-        r = requests.post(HIPCHAT_API_URL, msg, params=payload)
-        print 'Message sent to Hack: %s\nResponse: %s' % (msg, r.text)
-    except:
+        r = requests.post(HIPCHAT_API_URL, message, params=payload)
+        print 'Message sent to HipChat: %s\nResponse: %s' % (message, r.text)
+    except Exception:
         print sys.exc_info()[1]
+
+
+if __name__ == '__main__':
+    deployer = sys.argv[1] if len(sys.argv) >= 2 else "rmc"
+
+    notify_hipchat(deployer, secrets.HIPCHAT_HACK_ROOM_ID)
+    notify_hipchat(deployer, secrets.HIPCHAT_PUBLIC_ROOM_ID)
