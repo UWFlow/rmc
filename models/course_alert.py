43e32014b72ccc258a53873dabe9621e9958883d
David Hu <davidhu91@gmail.com>
2014-04-03 06:00:41 -0400

    Make CourseAlert an ABC; add methods to send and delete

--- models/course_alert.py
+++ models/course_alert.py
@@ -1,29 +1,30 @@
+import datetime
+
 import mongoengine as me
 
-from rmc.shared import util
+import section
 
 
-class CourseAlert(me.Document):
-    """A request to be notified for when a seat opens in a course.
+class BaseCourseAlert(me.Document):
+    """An abstract base class for notifying when a seat opens in a course.
 
-    Can optionally specify a specific section of a course.
+    Subclasses must define the behaviour for sending the alert to the desired
+    audience.
+
+    Can optionally specify a single section of a course.
     """
 
     meta = {
         'indexes': [
             'course_id',
-            'user_id',
             ('course_id', 'term_id', 'section_type', 'section_num'),
         ],
+        'abstract': True,
     }
 
-    # The user to send this alert to
-    user_id = me.ObjectIdField(required=True,
-            unique_with=['course_id', 'term_id', 'section_type',
-                'section_num'])
-
     # eg. earth121l
-    course_id = me.StringField(required=True)
+    course_id = me.StringField(required=True,
+            unique_with=['term_id', 'section_type', 'section_num'])
 
     # eg. datetime.datetime(2013, 1, 7, 22, 30)
     created_date = me.DateTimeField(required=True)
@@ -34,25 +35,55 @@ class CourseAlert(me.Document):
     # Optional fields to specify section to alert on
 
     # eg. 2013_09. Note that this is our term ID, not Quest's 4-digit ID.
-    term_id = me.StringField()
+    term_id = me.StringField(default='')
 
     # eg. LEC, TUT, EXAM. Note uppercase.
-    section_type = me.StringField()
+    section_type = me.StringField(default='')
 
     # eg. 001
-    section_num = me.StringField()
+    section_num = me.StringField(default='')
 
-    TO_DICT_FIELDS = ['id', 'user_id', 'course_id', 'created_date',
-            'expiry_date', 'term_id', 'section_type', 'section_num']
+    def send_alert(self, sections):
+        """Sends an alert about a seat opening.
 
-    def to_dict(self):
-        return util.to_dict(self, CourseAlert.TO_DICT_FIELDS)
+        Args:
+            sections: Sections that have spots available.
 
-    def __repr__(self):
-        return '<CourseAlert: %s, %s, %s, %s %s>' % (
-            self.user_id,
-            self.course_id,
-            self.term_id,
-            self.section_type,
-            self.section_num,
-        )
+        Returns whether this alert was successfully sent.
+        """
+        raise Exception('Sublasses must implement this method.')
+
+    @classmethod
+    def try_send_alerts(cls):
+        """Checks if any alerts can be sent, and if so, sends them.
+
+        Returns how many alerts were successfully sent.
+        """
+        alerts_sent = 0
+
+        for alert in cls.objects():
+            query = {'course_id': alert.course_id}
+
+            if alert.term_id:
+                query['term_id'] = alert.term_id
+
+            if alert.section_type:
+                query['section_type'] = alert.section_type
+
+            if alert.section_num:
+                query['section_num'] = alert.section_num
+
+            sections = section.Section.objects(**query)
+            open_sections = filter(
+                    lambda s: s.enrollment_capacity > s.enrollment_total,
+                    sections)
+
+            if open_sections and alert.send_alert(open_sections):
+                alert.delete()
+                alerts_sent += 1
+
+        return alerts_sent
+
+    @classmethod
+    def delete_expired(cls):
+        cls.objects(expiry_date__gt=datetime.datetime.now()).delete()
