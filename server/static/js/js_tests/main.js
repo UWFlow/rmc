293710fb4bb1fc3bd6ea4ed91efd2bd9d4156f8f
JGulbronson <jgulbronson@gmail.com>
2014-11-30 19:27:11 -0500

    Added tests for new heuristics

--- server/static/js/js_tests/main.js
+++ server/static/js/js_tests/main.js
@@ -18,6 +18,7 @@ require(['../config_settings'], function(config_settings) {
     require([
       'transcript/basic_test.js',
       'schedule/basic_test.js',
+      'schedule/basic_test_different_date_format.js',
       'schedule/tba_date_test.js',
       'schedule/prof_name_test.js'
     ], function(require) {
